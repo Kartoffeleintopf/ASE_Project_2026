ecbffca40b786017c4402a8fa41b36ac5289c0cd
Kartoffeleintopf <cedric-noe@mail.de>
2026-01-29 19:30:53 +0100

    first commit

---
+++ src/main/java/com/example/ase_project_2026/AseProject2026Application.java
@@ -0,0 +1,13 @@
+package com.example.ase_project_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AseProject2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AseProject2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ase_project_2026/AseProject2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ase_project_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseProject2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
