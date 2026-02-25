22dc87d7265645f7619e65ad099a71e12ba8735a
Kartoffeleintopf <cedric-noe@mail.de>
2026-02-25 09:54:53 +0100

    spelling mistake

--- src/main/java/com/example/ase_project_2026/Recipe.java
+++ src/main/java/com/example/ase_project_2026/Recipe.java
@@ -1,6 +1,6 @@
 package com.example.ase_project_2026;
 
 public class Recipe {
-    // how to put ingredients in here with there associated amount?
+    // how to put ingredients in here with their associated amount?
     //should this even be a datatype??
 }
