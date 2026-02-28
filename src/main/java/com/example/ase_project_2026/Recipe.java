4fd83f4115c94eb9237d853c887ff29a2096afc8
Kartoffeleintopf <cedric-noe@mail.de>
2026-02-28 15:13:52 +0100

    commented out until furhter development

--- src/main/java/com/example/ase_project_2026/Recipe.java
+++ src/main/java/com/example/ase_project_2026/Recipe.java
@@ -1,6 +1,32 @@
 package com.example.ase_project_2026;
+/*
+import jakarta.persistence.*;
 
+@Entity
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
     // how to put ingredients in here with their associated amount?
+    // probably via ids
     //should this even be a datatype??
+    protected Recipe() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+*/
