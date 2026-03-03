a83326ae1bb09f5fae9c7db46b434f49fd3ea6b0
Kartoffeleintopf <cedric-noe@mail.de>
2026-03-03 12:41:44 +0100

    added ToDO for more classification of methods
    added Ingredient delete method

--- src/main/java/com/example/ase_project_2026/IngredientController.java
+++ src/main/java/com/example/ase_project_2026/IngredientController.java
@@ -27,10 +27,18 @@ public class IngredientController {
     @GetMapping("/name/{ingredientID}")
     public String getIngredientNameByID(@PathVariable long ingredientID) {
         return Objects.requireNonNull(repository.findById(ingredientID).orElse(null)).getName();
+        // look at this snippet again, it seems stupid
     }
 
+    @DeleteMapping("/delete/{ingredientID}")
+    public void deleteIngredientByID(@PathVariable long ingredientID) {
+        repository.deleteById(ingredientID);
+        //check for exsitence
+    }
     // ToDO
     /*
+    API design: more clasification needed: mabye speratre into actions or something or get nspiration
+
     Zutaten bearbeiten oder loeschen
 
     Rezept produzieren (maybe need another controller for that?!
