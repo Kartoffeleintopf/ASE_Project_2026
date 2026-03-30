d4a512c0fc2fec382fff926a09b6ce1fb22e4ba4
Kartoffeleintopf <cedric-noe@mail.de>
2026-03-30 13:00:12 +0200

    removed redundant import

--- domain/src/main/java/ase/recipe/RecipeRepository.java
+++ domain/src/main/java/ase/recipe/RecipeRepository.java
@@ -1,6 +1,5 @@
 package ase.recipe;
 
-import ase.recipe.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
