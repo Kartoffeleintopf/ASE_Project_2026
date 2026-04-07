bf558ecc007bc8639240966186e6fa15981572e1
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-07 15:54:18 +0200

    new method for deleting ingredients from the repository

--- domain/src/main/java/ase/ingredient/IngredientRepository.java
+++ domain/src/main/java/ase/ingredient/IngredientRepository.java
@@ -2,8 +2,9 @@ package ase.ingredient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     List<Ingredient> findByBase(boolean base);
-    List<Ingredient> findByName(String name);
+    Optional<Ingredient> findByName(String name);
 }
