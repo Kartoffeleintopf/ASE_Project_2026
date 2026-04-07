dbbc9e895ade6f6202b5ba3d4d8293c7eb206f1b
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-08 00:08:26 +0200

    do i need a method to make the check inside IngredientApplicationService work? or can cotains Ingredient from Recipe be used?

--- domain/src/main/java/ase/recipe/RecipeRepository.java
+++ domain/src/main/java/ase/recipe/RecipeRepository.java
@@ -1,8 +1,10 @@
 package ase.recipe;
 
+import ase.ingredient.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByNameContaining(String name);
+    //List<Recipe> ContainsIngredient(Ingredient ingredient);
 }
