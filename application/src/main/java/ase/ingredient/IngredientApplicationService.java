4018b257275357c4d8292d1417833d007e43afeb
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-07 23:48:25 +0200

    methods for create, update and (not yet) deleting ingredients. Also the methods for finding ingredients: all, by base or containing a string

--- application/src/main/java/ase/ingredient/IngredientApplicationService.java
+++ application/src/main/java/ase/ingredient/IngredientApplicationService.java
@@ -1,7 +1,7 @@
 package ase.ingredient;
 
-import ase.ingredient.Ingredient;
-import ase.ingredient.IngredientRepository;
+import ase.warehouse.WarehouseEntry;
+import ase.warehouse.WarehouseEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +10,43 @@ import java.util.List;
 @Service
 public class IngredientApplicationService {
     private final IngredientRepository ingredientRepository;
+    private final WarehouseEntryRepository warehouseEntryRepository;
 
     @Autowired
-    public IngredientApplicationService(IngredientRepository ingredientRepository) {
+    public IngredientApplicationService(IngredientRepository ingredientRepository,
+                                        WarehouseEntryRepository warehouseEntryRepository) {
         this.ingredientRepository = ingredientRepository;
+        this.warehouseEntryRepository = warehouseEntryRepository;
     }
-/*
+
+    public Ingredient createIngredient(String name, String picture, boolean base) {
+        Ingredient ingredient = ingredientRepository.save(new Ingredient(name, picture, base));
+        warehouseEntryRepository.save(new WarehouseEntry(ingredient));
+        return ingredient;
+    }
+
+    public Ingredient updateIngredient(long id, String name, String picture) {
+        Ingredient ingredient = ingredientRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Ingredient not found"));
+        ingredient.setName(name);
+        ingredient.setPicture(picture);
+        return ingredientRepository.save(ingredient);
+    }
+
+    public void deleteIngredient(long id) {
+        // ToDo
+        // Careful of interdependencies
+    }
+
     public List<Ingredient> findAllIngredients() {
-        return ingredientRepository.findAllIngredients();
-    }*/
+        return ingredientRepository.findAll();
+    }
+
+    public List<Ingredient> findByBase(boolean base) {
+        return ingredientRepository.findByBase(base);
+    }
+
+    public List<Ingredient> findByNameContaining(String name) {
+        return ingredientRepository.findByNameContaining(name);
+    }
 }
