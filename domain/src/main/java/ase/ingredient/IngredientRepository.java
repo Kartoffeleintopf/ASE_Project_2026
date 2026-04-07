eb999ef68c4420f61e45440804804b17ce1cbb40
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-07 23:37:28 +0200

    could return multiple Ingredients, thus changed from Optional to List

--- domain/src/main/java/ase/ingredient/IngredientRepository.java
+++ domain/src/main/java/ase/ingredient/IngredientRepository.java
@@ -2,9 +2,8 @@ package ase.ingredient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     List<Ingredient> findByBase(boolean base);
-    Optional<Ingredient> findByNameContaining(String name);
+    List<Ingredient> findByNameContaining(String name);
 }
