bc900db05c738dd8549f8a8af69c39773188dcd5
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-07 22:42:59 +0200

    renamed to better reflect function scope

--- domain/src/main/java/ase/recipe/RecipeRepository.java
+++ domain/src/main/java/ase/recipe/RecipeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    List<Recipe> findByName(String name);
+    List<Recipe> findByNameContaining(String name);
 }
