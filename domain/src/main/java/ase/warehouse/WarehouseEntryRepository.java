087acf2db843a2ea5c4466d17e25795af216722b
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-07 14:53:52 +0200

    added method for finding by Ingredient inside the WarehouseEntryRepository

--- domain/src/main/java/ase/warehouse/WarehouseEntryRepository.java
+++ domain/src/main/java/ase/warehouse/WarehouseEntryRepository.java
@@ -1,9 +1,15 @@
 package ase.warehouse;
 
+import ase.ingredient.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface WarehouseEntryRepository extends JpaRepository<WarehouseEntry, Long> {
+    // Ingredient method(s)
+    Optional<WarehouseEntry> findByIngredient(Ingredient ingredient);
+
+    // Amount methods
     List<WarehouseEntry> findByAmountGreaterThan(int amount);
     List<WarehouseEntry> findByAmount (int amount);
     List<WarehouseEntry> findByAmountLessThan(int amount);
