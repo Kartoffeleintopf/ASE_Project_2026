0597d5170d4ce40563cf43216764b742e34edfe8
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-08 19:53:04 +0200

    1.0 of WarehouseApplication Service

---
+++ application/src/main/java/ase/warehouse/WarehouseApplicationService.java
@@ -0,0 +1,67 @@
+package ase.warehouse;
+
+import ase.ingredient.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WarehouseApplicationService {
+    private final WarehouseEntryRepository warehouseEntryRepository;
+
+    @Autowired
+    public WarehouseApplicationService(WarehouseEntryRepository warehouseEntryRepository) {
+        this.warehouseEntryRepository = warehouseEntryRepository;
+    }
+
+    public List<WarehouseEntry> findAll() {
+        return warehouseEntryRepository.findAll();
+    }
+
+    public List<WarehouseEntry> findAllInStock() {
+        return warehouseEntryRepository.findByAmountGreaterThan(0);
+    }
+
+    public List<WarehouseEntry> findByAmount(int amount) {
+        return warehouseEntryRepository.findByAmount(amount);
+    }
+
+    public List<WarehouseEntry> findByAmountGreaterThan(int amount) {
+        return warehouseEntryRepository.findByAmountGreaterThan(amount);
+    }
+
+    public List<WarehouseEntry> findByAmountLessThan(int amount) {
+        return warehouseEntryRepository.findByAmountLessThan(amount);
+    }
+
+    public List<WarehouseEntry> findByAmountBetween(int start, int end) {
+        return warehouseEntryRepository.findByAmountGreaterThanAndAmountLessThan(start, end);
+    }
+
+    public WarehouseEntry findByIngredient(Ingredient ingredient) {
+        return warehouseEntryRepository.findByIngredient(ingredient)
+                .orElseThrow(() -> new IllegalArgumentException("Warehouse entry not found"));
+    }
+
+    public WarehouseEntry addAmount(long id, int amount) {
+        WarehouseEntry entry = warehouseEntryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Warehouse entry not found"));
+        entry.addAmount(amount);
+        return warehouseEntryRepository.save(entry);
+    }
+
+    public WarehouseEntry subtractAmount(long id, int amount) {
+        WarehouseEntry entry = warehouseEntryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Warehouse entry not found"));
+        entry.subtractAmount(amount);
+        return warehouseEntryRepository.save(entry);
+    }
+
+    public WarehouseEntry setAmount(long id, int amount) {
+        WarehouseEntry entry = warehouseEntryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Warehouse entry not found"));
+        entry.setAmount(amount);
+        return warehouseEntryRepository.save(entry);
+    }
+}
