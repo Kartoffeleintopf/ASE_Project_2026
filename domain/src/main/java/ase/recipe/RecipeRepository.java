966c838083d586890a782a3dad16e11c5a7d8884
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-08 17:45:57 +0200

    Method for finding recipes for specific ingredient

--- domain/src/main/java/ase/recipe/RecipeRepository.java
+++ domain/src/main/java/ase/recipe/RecipeRepository.java
@@ -3,7 +3,9 @@ package ase.recipe;
 import ase.ingredient.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByNameContaining(String name);
+    Optional<Recipe> findByProduce(Ingredient produce);
 }
