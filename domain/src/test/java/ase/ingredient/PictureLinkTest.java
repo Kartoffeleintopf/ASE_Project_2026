2b722657b70fd4529bc410753871b5ddf2044349
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-09 23:40:44 +0200

    tsting getURL and hashCode methods

--- domain/src/test/java/ase/ingredient/PictureLinkTest.java
+++ domain/src/test/java/ase/ingredient/PictureLinkTest.java
@@ -5,6 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PictureLinkTest {
+
+    @Test
+    void getUrl() {
+        PictureLink pictureLink = new PictureLink("link");
+        assertEquals("link", pictureLink.getUrl());
+    }
+
     @Test
     void pictureLinkEquality() {
         PictureLink a = new PictureLink("www.link.net");
@@ -17,5 +24,12 @@ class PictureLinkTest {
         PictureLink a = new PictureLink("www.link1.net");
         PictureLink b = new PictureLink("www.link2.net");
         assertNotEquals(a, b);
+    }
+
+    @Test
+    void hashCodeConsistentWithEquals() {
+        PictureLink a = new PictureLink("link");
+        PictureLink b = new PictureLink("link");
+        assertEquals(a.hashCode(), b.hashCode());
     }
 }
