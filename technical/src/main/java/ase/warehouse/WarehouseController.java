1b26abd214e7399aac170d2e3eb0cad635335234
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-09 20:52:05 +0200

    warehouseController with findAll method

---
+++ technical/src/main/java/ase/warehouse/WarehouseController.java
@@ -0,0 +1,25 @@
+package ase.warehouse;
+
+import ase.ingredient.IngredientApplicationService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/warehouse")
+public class WarehouseController {
+
+    private final WarehouseApplicationService warehouseApplicationService;
+    private final IngredientApplicationService ingredientApplicationService;
+
+    public WarehouseController(WarehouseApplicationService warehouseApplicationService,
+                               IngredientApplicationService ingredientApplicationService) {
+        this.warehouseApplicationService = warehouseApplicationService;
+        this.ingredientApplicationService = ingredientApplicationService;
+    }
+
+    @GetMapping("/all")
+    public List<WarehouseEntry> findAll() {
+        return warehouseApplicationService.findAll();
+    }
+}
