2b0515a7e04562bf7c45540fbd06cc8ab9ee15d3
Kartoffeleintopf <cedric-noe@mail.de>
2026-04-10 00:01:07 +0200

    Mock Test setup

---
+++ application/src/test/java/ase/ingredient/IngredientApplicationServiceTest.java
@@ -0,0 +1,52 @@
+package ase.ingredient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientApplicationServiceTest {
+
+    // ATRIP
+    /*
+    A utomatic
+    T horough
+    R epeatable
+    I ndependant
+    P rofessional
+     */
+
+    @Test
+    void createIngredient() {
+        // ToDo
+    }
+
+    @Test
+    void updateIngredient() {
+        // ToDo
+    }
+
+    @Test
+    void deleteIngredient() {
+        // ToDo
+    }
+
+    @Test
+    void findAllIngredients() {
+        // ToDo
+    }
+
+    @Test
+    void findIngredientById() {
+        // ToDo
+    }
+
+    @Test
+    void findByBase() {
+        // ToDo
+    }
+
+    @Test
+    void findByNameContaining() {
+        // ToDo
+    }
+}
